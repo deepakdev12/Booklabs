47b59c825444189c05064c69d550ca8a8f3e7c14
deepakdev12 <deepakdharmode12@gmail.com>
2025-01-21 11:50:46 +0530

    Done

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            Explore a diverse range of courses at
+            <span className="text-pink-500"> BookLabs,</span> covering multiple
+            genres to suit every reader’s passion. From thrilling mysteries and
+            timeless classics to insightful self-improvement and cutting-edge
+            science fiction, our courses help you dive deeper into the world of
+            literature. Whether you're a beginner or a seasoned reader, there's
+            something for everyone! 📚 Check out our latest book collections
+            below and start your literary journey today. New books and exciting
+            reads are added regularly—discover your next favorite now! 🚀
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item._id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
